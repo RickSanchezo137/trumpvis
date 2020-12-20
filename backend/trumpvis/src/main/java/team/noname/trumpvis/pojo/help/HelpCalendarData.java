3369e1ccdcb29c8a0a2925b47fdf620a0a97c8f3
hufei <747591245@qq.com>
2020-12-20 11:08:58 +0800

    先不用redis缓存，用的内存缓存，方便调试 写了一下calendar的逻辑

---
+++ backend/trumpvis/src/main/java/team/noname/trumpvis/pojo/help/HelpCalendarData.java
@@ -0,0 +1,22 @@
+package team.noname.trumpvis.pojo.help;
+
+public class HelpCalendarData {
+    private String time;
+    private Integer count;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
